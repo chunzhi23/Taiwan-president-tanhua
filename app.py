1036e10d0ee2089ede7c45ccb8a9184a1ca058c8
chunzhi23 <lzhongm05@gmail.com>
2022-05-30 17:52:30 +0900

    [trivial] correct get method

--- app.py
+++ app.py
@@ -11,16 +11,21 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/api/articles', methods=['GET', 'POST'])
+@app.route('/NEWS/<int:id>')
+def news(id):
+    return render_template('news.html')
+
+@app.route('/api/articles', methods=['GET'])
 def get_articles():
-    if request.method == 'POST':
-        data = request.get_json()
-        if data['action'] == 'new':
-            subprocess.run('go run main.go', shell=True, stdout=subprocess.PIPE)
+    data = request.args.get("action")
+    if data == 'new':
+        subprocess.run('go run main.go', shell=True, stdout=subprocess.PIPE)
 
     df = pd.read_csv('data/articles.csv')
     df = df.iloc[natsort.index_humansorted(df['date'])]
     return json.dumps(df.to_dict(orient='records'), ensure_ascii=False)
+
+# @app.route('/api/details', methods=['GET'])
 
 if __name__ == '__main__':
     app.run(debug=True)
