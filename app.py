be101dafb531507a19388b9b0f354b32ab3ce72e
chunzhi23 <lzhongm05@gmail.com>
2022-05-30 18:46:32 +0900

    tanhua get detail

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-import json
+import json, requests
 import natsort
 import subprocess
 import pandas as pd
 
 from flask import Flask, render_template, request
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
@@ -17,7 +18,7 @@ def news(id):
 
 @app.route('/api/articles', methods=['GET'])
 def get_articles():
-    data = request.args.get("action")
+    data = request.args.get('action')
     if data == 'new':
         subprocess.run('go run main.go', shell=True, stdout=subprocess.PIPE)
 
@@ -25,7 +26,28 @@ def get_articles():
     df = df.iloc[natsort.index_humansorted(df['date'])]
     return json.dumps(df.to_dict(orient='records'), ensure_ascii=False)
 
-# @app.route('/api/details', methods=['GET'])
+@app.route('/api/details', methods=['GET'])
+def get_details():
+    data = request.args.get('id')
+    url = 'https://www.president.gov.tw/NEWS/%s' % data
+    req = requests.get(url)
+    soup = BeautifulSoup(req.text, 'html.parser')
+
+    tb = soup.select('#wrapper > section > div > div.words > div.president > div > div.col-sm-8')[0]
+    divs = tb.find_all('div', recursive=False)
+    subtitle = divs[0].get_text().strip()
+    date = divs[1].get_text().strip()
+    
+    content = ''
+    ps = tb.find_all('p')
+    for p in ps:
+        content += p.get_text().strip()
+
+    return json.dumps({
+        'subtitle': subtitle,
+        'date': date,
+        'content': content
+    }, ensure_ascii=False)
 
 if __name__ == '__main__':
     app.run(debug=True)
